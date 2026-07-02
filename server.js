2c0a0b715f96c9e77baa238b81381126d67a1ae8
Jaures Guedou <sewlanguedou@gmail.com>
2026-07-02 15:23:04 +0100

    First Express App

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'src/views'));
 
