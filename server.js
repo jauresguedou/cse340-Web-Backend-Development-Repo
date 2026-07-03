3463920d94286de1822660cae679d26de9ed2a04
Jaures Guedou <sewlanguedou@gmail.com>
2026-07-03 13:09:17 +0100

    Troubleshoot of route

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.get('/categories', async(req,res) =>{
   res.render('categories', {title});
 });
 
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);
